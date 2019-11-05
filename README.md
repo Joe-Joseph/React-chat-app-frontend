e466cbcbbabaa5fa0f69cceaa4de4e57185d54db
Joseph <44877921+Joe-Joseph@users.noreply.github.com>
2019-11-05 10:26:41 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
 ## Chat Application
 
 People in the same room can chat
---
+++ build/precache-manifest.2f9335d684c5a64cc4c758a609a6bd9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a2f031fe778e035086e61c287aea27c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47a1eb5ac712313ef959",
+    "url": "/static/css/main.9354cede.chunk.css"
+  },
+  {
+    "revision": "48c098f43b2722f67fd4",
+    "url": "/static/js/2.7faabbc2.chunk.js"
+  },
+  {
+    "revision": "47a1eb5ac712313ef959",
+    "url": "/static/js/main.ad9d6462.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
